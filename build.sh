b4b66c13a723da66f8a29396d60ca665f26eb716
Chans <imagechans@gmail.com>
2024-01-29 03:52:40 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,6 +21,7 @@ echo =================================
 echo  安装 rust
 echo =================================
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+echo 1
 source $HOME/.cargo/env
 rustup default stable
 rustup update
