f9088c426467ae2e80151df9d8e25d18f0bf579d
Chans <imagechans@gmail.com>
2024-01-29 03:22:30 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ echo =================================
 
 sudo apt-get update
 sudo apt-get upgrade -y
+sudo apt install build-essential -y
+sudo apt install --assume-yes git clang curl libssl-dev llvm libudev-dev make protobuf-compiler
