abdb59d2abc3fa4b2ce5c850223c27bde6a0a9f6
Chans <imagechans@gmail.com>
2024-01-29 03:43:20 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,31 @@ echo =================================
 echo 自动化部署开始执行
 echo =================================
 
+
+echo =================================
+echo  正在更新系统文件
+echo =================================
 sudo apt-get update
 sudo apt-get upgrade -y
-echo ok
+echo =================================
+echo  安装 build-essential
+echo =================================
 sudo apt install build-essential -y
-echo ok
+echo =================================
+echo  安装 git clang curl libssl-dev llvm libudev-dev make protobuf-compiler
+echo =================================
 sudo apt install --assume-yes git clang curl libssl-dev llvm libudev-dev make protobuf-compiler
-echo ok
+echo =================================
+echo  安装 rust
+echo =================================
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
+rustup default stable
+rustup update
+rustup update nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup target add wasm32-unknown-unknown
+echo =================================
+echo  安装 d9-node
+echo =================================
+git clone https://github.com/D-Nine-Chain/d9_node.git && cd d9_node && cargo build --release
