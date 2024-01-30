663d919e38fe7653e7df6422145396a790771868
Chans <imagechans@gmail.com>
2024-01-31 01:26:50 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 cd ~/d9_node && cargo build --release
 git clone https://github.com/iMageChans/ddddddddd
-sudo cp ddddddddd/d9-node.service /etc/systemd/system/
+cp ~/d9_node/ddddddddd/main-spec.json ~/d9_node/
+sudo cp ~/d9_node/ddddddddd/d9-node.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable d9-node.service
 sudo systemctl start d9-node.service; journalctl -u d9-node.service -f
