0b0c12ced06e37e92a48be5d42abe651b61bf48a
Chans <imagechans@gmail.com>
2024-02-29 23:52:20 +0800

    123

--- rest_node.sh
+++ rest_node.sh
@@ -1,5 +1,6 @@
 sudo systemctl stop d9-node.service
 cp ddddddddd/new-main-spec.json d9_node/main-spec.json
+sudo cp ~/ddddddddd/d9-node.service ~/d9_node/d9-node.service
 sudo cp ~/d9_node/d9-node.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start d9-node.service
